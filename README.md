0adcc12df8b03b1041c460232c6469dd45bf65a6
krzysztofkarolak <krzysiek.karolak1@gmail.com>
2025-02-11 20:18:55 +0100

    Add Hilink support

--- README.md
+++ README.md
@@ -1,4 +1,8 @@
-# SMS Gateway for Pub/Sub and SSH (Openwrt)
+# SMS Gateway for Pub/Sub
+
+Currently it supports sending SMS received from Pub/Sub subscription by:
+- SSH (using Openwrt sms_tool)
+- Hilink API
 
 ### Contributing
 Contributions are welcome! Please submit a pull request or open an issue to discuss any changes.
--- image/sms.py
+++ image/sms.py
@@ -1,6 +1,7 @@
 import os
 import io
 import paramiko
+import subprocess
 from google.cloud import pubsub_v1
 from google.oauth2 import service_account
 import json
@@ -30,20 +31,21 @@ subscription_id = os.environ.get("PUBSUB_SUBSCRIPTION_NAME")
 timeout = None
 lock = threading.Lock()
 
-# SSH settings
+# Connection settings
 ssh_host = os.environ.get("SSH_HOST")
 ssh_port = 22
 ssh_user = os.environ.get("SSH_USER")
 ssh_private_key = os.environ.get("SSH_PRIVATE_KEY")
 modem_port = os.environ.get("MODEM_PORT")
+send_mode = os.environ.get("SEND_MODE")
 char_limit = int(os.environ.get("CHAR_LIMIT"))
-if not ssh_private_key:
+if not ssh_private_key and send_mode == "ssh":
     raise ValueError("SSH private key not found in the environment variable.")
 
 def shrink_string(s):
     return s[:char_limit] + '...' if len(s) > char_limit else s
 
-def process_message(phone_number, text_message):
+def process_message_ssh(phone_number, text_message):
     try:
         ssh_client = paramiko.SSHClient()
         ssh_client.load_system_host_keys()
@@ -64,6 +66,7 @@ def process_message(phone_number, text_message):
             logging.info(f"Command output: {output}")
         if error:
             logging.error(f"Command error: {error}")
+            return False
 
         ssh_client.close()
         return error == ""
@@ -73,7 +76,27 @@ def process_message(phone_number, text_message):
     except Exception as e:
         logging.error(f"Error processing the message: {e}")
         return False
+    
+def process_message_hilink(phone_number, text_message):
+    command = f"hlcli smssend -to={phone_number} -msg='{text_message}'"
+    try:
+        result = subprocess.run(command, shell=True, check=True, text=True, capture_output=True)
 
+        output = result.stdout.strip()
+        error = result.stderr.strip()
+
+        if output:
+            logging.info(f"Success:\n{output}")
+        if error:
+            logging.error(f"Error:\n{error}")
+
+        return not error
+    except subprocess.CalledProcessError as e:
+        logging.error(f"Command failed with error:\n{e.stderr}")
+        return False
+    except Exception as e:
+        logging.error(f"Unexpected error: {e}")
+        return False
 
 def callback(message):
     with lock:
@@ -85,6 +108,7 @@ def callback(message):
             if phone_number and text_message:
                 logging.info(f"Received message for phone number: {phone_number}")
 
+                process_message = process_message_hilink if send_mode == "hilink" else process_message_ssh
                 success = process_message(phone_number, text_message)
                 if success:
                     message.ack()  # Acknowledge the message after successful processing
